be3a852390cda9fa14df031da847747065c54b5a
lwolanin <l.wolanin@stud.elka.pw.edu.pl>
2020-05-24 12:42:01 +0200

    Added Open GL to project

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,92 @@
-#include <iostream>
-#include <sstream>
-#include "Reader.h"
-#include "Scanner.h"
-#include "Parser.h"
-#include "Error.h"
+//#include <iostream>
+//#include <sstream>
+//#include "Reader.h"
+//#include "Scanner.h"
+//#include "Parser.h"
+//#include "Error.h"
+//
+//using namespace tkom;
+//
+//int main()
+//{
+//	std::stringstream in{ "//Sample script\nint main()\n{\nint i = 7;\nreturn i;\n}\n" };
+//	Reader reader{ in };
+//	Scanner scanner{ reader };
+//	Parser parser{ scanner };
+//
+//	std::unique_ptr<ast::Program> program;
+//
+//	try
+//	{
+//		program = parser.parse();
+//	}
+//	catch (const Error& e)
+//	{
+//		std::cout << e.what() << std::endl;
+//	}
+//
+//	return 0;
+//}
 
-using namespace tkom;
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+using namespace std;
+
+const GLuint WIDTH = 800, HEIGHT = 600;
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	cout << key << endl;
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
 
 int main()
 {
-	std::stringstream in{ "//Sample script\nint main()\n{\nint i = 7;\nreturn i;\n}\n" };
-	Reader reader{ in };
-	Scanner scanner{ reader };
-	Parser parser{ scanner };
-
-	std::unique_ptr<ast::Program> program;
-
+	if (glfwInit() != GL_TRUE)
+	{
+		cout << "GLFW initialization failed" << endl;
+		return -1;
+	}
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 	try
 	{
-		program = parser.parse();
+		GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "GKOM - OpenGL 01", nullptr, nullptr);
+		if (window == nullptr)
+			throw exception("GLFW window not created");
+		glfwMakeContextCurrent(window);
+		glfwSetKeyCallback(window, key_callback);
+
+		glewExperimental = GL_TRUE;
+		if (glewInit() != GLEW_OK)
+			throw exception("GLEW Initialization failed");
+
+		glViewport(0, 0, WIDTH, HEIGHT);
+
+		// main loop
+		while (!glfwWindowShouldClose(window))
+		{
+			// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
+			glfwPollEvents();
+
+			// Clear the colorbuffer
+			glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
+
+			// Swap the screen buffers
+			glfwSwapBuffers(window);
+		}
 	}
-	catch (const Error& e)
+	catch (exception ex)
 	{
-		std::cout << e.what() << std::endl;
+		cout << ex.what() << endl;
 	}
+	glfwTerminate();
 
 	return 0;
 }
